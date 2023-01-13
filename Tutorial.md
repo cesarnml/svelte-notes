3735717647486b4bef59ee983c022a20ebba2672
Cesar Mejia <cmejia@gmail.com>
2023-01-13 20:42:09 +0700

    initial commit

---
+++ Tutorial.md
@@ -0,0 +1,6 @@
+# Svelte Tutorial Notes
+
+- [Svelte Tutorial Notes](#svelte-tutorial-notes)
+  - [Section 01](#section-01)
+
+## Section 01
---
+++ tutorial-svelete-app/src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const m = {
+    x: 0,
+    y: 0,
+  }
+</script>
+
+<button
+  on:mousemove={(e) => {
+    m.x = e.clientX
+    m.y = e.clientY
+  }}
+>
+  the mouse position is ({m.x}, {m.y})
+</button>
+<div>base</div>
